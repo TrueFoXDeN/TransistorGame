99467dbe56e3347579eb097ca7d65eaf92966174
laurenz <laurenz.dilba@hotmail.de>
2024-06-02 20:52:28 +0200

    hitboxes for pmos

--- PMOS.gd
+++ PMOS.gd
@@ -14,13 +14,11 @@ func _process(delta):
 	if draggable:
 		if Input.is_action_just_pressed("click"):
 			Global.dragging = true
-			print('set dragging true')
 			offset = get_global_mouse_position() - global_position
 		if Input.is_action_pressed("click"):
 			global_position = get_global_mouse_position() - offset
 		if Input.is_action_just_released("click"):
 			Global.dragging = false
-			print('set dragging false')
 
 
 func _on_mouse_entered():
@@ -35,3 +33,27 @@ func _on_mouse_exited():
 	draggable = false
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1).set_ease(Tween.EASE_OUT)
+
+
+func _on_gate_mouse_entered():
+	print('gate entered')
+
+
+func _on_gate_mouse_exited():
+	print('gate exited')
+
+
+func _on_source_mouse_entered():
+	print('source entered')
+
+
+func _on_source_mouse_exited():
+	print('source exited')
+
+
+func _on_drain_mouse_entered():
+	print('drain entered')
+
+
+func _on_drain_mouse_exited():
+	print('drain exited')
